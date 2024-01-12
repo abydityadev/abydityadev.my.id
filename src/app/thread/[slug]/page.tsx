cd5fa5cf46fbc20c257e210c8dfea1e2b8e9f21a
abydityadev <abyan.raditya@icloud.com>
2024-01-12 09:53:51 +0700

    🔨 Fix blog error while rendering

--- src/app/thread/[slug]/page.tsx
+++ src/app/thread/[slug]/page.tsx
@@ -1,39 +1,58 @@
-import React from 'react'
 import { allPosts } from "contentlayer/generated";
-import { getMDXComponent } from 'next-contentlayer/hooks';
-import Footer from '@/components/Footer/Footer';
+import { notFound } from "next/navigation";
+import { useMDXComponent } from "next-contentlayer/hooks";
 import type { MDXComponents } from "mdx/types";
-import { BlogNav } from '@/components/Navbar/Navigation';
-import Divider from '@/components/Divider';
 
+import Link from "next/link";
+import { BlogNav } from "@/components/Navbar/Navigation";
+import Footer from "@/components/Footer/Footer";
+import Divider from "@/components/Divider";
 
-const mdxComponents: MDXComponents = {};
+const mdxComponents: MDXComponents = {
+    a: ({ href, children }) => <Link href={href as string}>{children}</Link>,
+};
 
-export default function Page({ params }: { params: { slug: string } }) {
+const PostLayout = ({ params }: { params: { slug: string } }) => {
     const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
-    const Content = getMDXComponent(post?.body.code)
+    if (!post) notFound();
+
+    const MDXContent = useMDXComponent(post.body.code);
 
     return (
         <main>
-            <BlogNav />
+            <BlogNav post={post.title} />
             <section className='max-w-3xl mx-auto px-3 xl:px-0'>
                 <section className="my-16">
-                    <header >
-                        <p className='text-xs clear-start flex gap-3'>thread <span>/</span> {new Intl.DateTimeFormat("en-US", { dateStyle: 'long' }).format(new Date(post?.date))}</p>
-                        <h1 className='sm:text-8xl text-4xl font-black'>{post?.title}</h1>
-                        <p className='flex gap-5'>{post?.readTimeMinutes} min reads <span className='text-indigo-500'>{post?.tags}</span></p>
+                    <header>
+                        <div className="flex gap-3 items-center text-xs text-neutral-600 dark:text-neutral-400">
+                            <span>Thread</span>
+                            /
+                            <time dateTime={post.date}>
+                                {new Intl.DateTimeFormat("en-US", { dateStyle: "long" }).format(new Date(post.date))}
+                            </time>
+                        </div>
+                        <h1 className="text-6xl font-black my-2">{post.title}</h1>
+                        <p>{post.readTimeMinutes} min read <span className="text-neutral-500">@</span> <span className="text-indigo-500">{post.tags}</span></p>
                     </header>
                     <Divider />
-                    <article className='prose dark:prose-invert xl:prose-xl'>
-                        <Content components={mdxComponents} />
+                    <article className="prose dark:prose-invert xl:text-xl">
+                        <MDXContent components={mdxComponents} />
                     </article>
                 </section>
             </section>
             <hr className='pb-10  border-t border-t-neutral-700' />
             <Footer />
         </main >
-    )
-}
+    );
+};
+
+export default PostLayout;
 
 export const generateStaticParams = async () =>
     allPosts.map((post) => ({ slug: post._raw.flattenedPath }));
+
+export const generateMetadata = ({ params }: { params: { slug: string } }) => {
+    const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
+    if (!post) notFound();
+    return { title: post.title };
+};
--- src/components/Navbar/Navigation.tsx
+++ src/components/Navbar/Navigation.tsx
@@ -80,7 +80,7 @@ export function Navigation() {
     )
 }
 
-export function BlogNav() {
+export function BlogNav({ post }: { post: string }) {
     const pathname = usePathname();
     const { resolvedTheme, theme, setTheme } = useTheme();
     const [Mounted, setMounted] = useState(false);
@@ -100,6 +100,7 @@ export function BlogNav() {
                 {/* Navbar name */}
                 <Link href={'/thread'}>← Back</Link>
 
+                <p className='hidden sm:block'>Thread / {post}</p>
                 {/* Navbar utilities */}
                 <ul className='flex items-center gap-3'>
                     <li>
@@ -110,33 +111,8 @@ export function BlogNav() {
                             {resolvedTheme == 'light' ? <Moon /> : <Sun />}
                         </button>
                     </li>
-                    <li>
-                        {/* Burger button for mobile */}
-                        <button
-                            onClick={() => setOpen(prev => !prev)}
-                            className='p-2 rounded duration-200 hover:ring-4 ring-neutral-200 dark:ring-neutral-800 block sm:hidden'>
-                            <Text />
-                        </button>
-                    </li>
                 </ul>
             </div>
-            {open && (
-                <div className='block sm:hidden absolute'>
-                    <div className='bg-light dark:bg-dark duration-300 w-screen h-screen px-4 xl:px-0'>
-                        <ul className='py-8'>
-                            {Config.map((path, idx) => {
-                                return (
-                                    <li className='py-3' key={idx}>
-                                        <Link className={pathname == path.path ? 'text-4xl font-bold text-neutral-500' : 'text-4xl font-bold'} href={path.path}>{path.label}</Link>
-                                        <hr className='border-t border-t-neutral-400 dark:border-t-neutral-700 my-2' />
-                                    </li>
-                                )
-                            })}
-                        </ul>
-                    </div>
-                </div>
-            )
-            }
         </nav >
     )
 }
